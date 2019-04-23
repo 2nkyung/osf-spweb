f39f0da158f6abca4bb320e677479771708914bd
2nkyung <92daisychoi@gmail.com>
2019-04-23 10:38:13 +0900

    파이널네임 변경

--- src/main/java/com/osf/web/controller/Exam.java
+++ src/main/java/com/osf/web/controller/Exam.java
@@ -1,14 +1,35 @@
 package com.osf.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Exam {
 
-	public String name;
+	private final static Logger logger = LoggerFactory.getLogger(Exam.class);
+	public String str = "11";
 	
+	/*
+	 * Log4j level 순서
+	 * 1. TRACE 상세한 정보
+	 * 2. DEBUG 개발할 때 필요한거
+	 * 3. INFO 정보(인포메이션) 어떤 프로젝트가 톰캣에서 돌아가고있다 정도
+	 * 4. WARN 워닝(동작은 하지만 위험하다)
+	 * 5. ERROR 실제 에러 ( 500에러, 서버 이터널 에러 등등)
+	 * 6. FATAL 치명적인 에러(서버가 중단되는 것 등등)
+	 */
 	public static void main(String[] args) {
-		Exam e = new Exam();
-		if(e.name.equals("홍길동")) {
-			System.out.println("이름은 홍길동이 맞습니다.");
+		String str = null;
+
+		if(str.equals("홍길동")) {
+			log.info("이름은 홍길동이 맞습니다.");
+		}else {
+			log.debug("나도 나올려나?");
+			log.info("이름은 홍길동이 아닙니다.");
 		}
+		log.warn("프로그램 비정상 종료, 그치만 문제 없음.");
 
 	}
 
