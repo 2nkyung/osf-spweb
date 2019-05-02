ee0020419632e5563dc6f417faade11cf521b820
2nkyung <92daisychoi@gmail.com>
2019-05-02 17:51:24 +0900

    5/2수업

--- src/main/java/com/osf/web/controller/EMPController.java
+++ src/main/java/com/osf/web/controller/EMPController.java
@@ -1,14 +1,13 @@
 package com.osf.web.controller;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,27 +20,26 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @Slf4j
 public class EMPController {
-
 	@Autowired
 	private EmpService es;
 
-	@RequestMapping(value = "/emp/login", method = RequestMethod.POST)
-	public @ResponseBody Map<String, String> doLogin(@RequestParam Map<String, String> params, HttpSession hs) {
-		Map<String, String> rMap = new HashMap<String, String>();
+	@RequestMapping(value="/emp/login", method=RequestMethod.POST)
+	public @ResponseBody Map<String,String> doLogin(@RequestParam Map<String,String> params
+			,HttpSession hs) {
+		Map<String,String> rMap = new HashMap<String,String>();	
 		rMap.put("result", "false");
-		if (es.loginEmp(params, hs)) {
+		if(es.loginEmp(params, hs)) {
 			rMap.put("result", "true");
 		}
 		return rMap;
 	}
+	
 
-	@RequestMapping("/emp/json")
-	public @ResponseBody List<String> doJson() {
-		List<String> list = new ArrayList<String>();
-		list.add("1");
-		list.add("2");
-		list.add("3");
-		list.add("4");
-		return list;
+	@RequestMapping(value="/emp/insert", method=RequestMethod.GET)
+	public String goInsert(Model m) {
+		m.addAttribute("deptList",es.selectDeptList());
+		return "/uri/emp/insert"; 
 	}
 }
+
+
--- src/main/webapp/WEB-INF/uri/index.jsp
+++ src/main/webapp/WEB-INF/uri/index.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!doctype html>
 <html lang="en">
 <head>
@@ -16,27 +16,39 @@
 <!-- Custom styles for this template -->
 <link href="/resources/signin.css" rel="stylesheet">
 </head>
+<script>
+function goEmpInsertPage(){
+	location.href = '/emp/insert';
+}
+</script>
 <body class="text-center">
-	<div class="container">${emp.ENAME}님 환영합니다.
-	<c:if test="${emp.LVL==2}">
+	<!--${empList}  -->
+	<div class="container">${emp.ENAME}님
+		환영합니다.
+		<c:if test="${emp.LVL==2}">
 	안녕하세요 사장님~ 사장님만 볼 수 있는 사원 리스트입니다.
 	<table class="table">
-	<tr>
-	<th>사번</th>
-	<th>이름</th>
-	<th>아이디</th>
-	<th>비밀번호</th>
-	</tr>
-	<c:forEach items="#{empList}" var="e">
-	<tr>
-	<td>${e.EMPNO}</td>
-	<td>${e.ENAME}</td>
-	<td>${e.ID}</td>
-	<td>${e.PWD}</td>
-	</tr>
-	</c:forEach>
-	</table>
-	</c:if>
+				<tr>
+					<th>사번</th>
+					<th>이름</th>
+					<th>아이디</th>
+					<th>비밀번호</th>
+					<th>커미션</th>
+				</tr>
+				<c:forEach items="#{empList}" var="e">
+					<tr>
+						<td>${e.empNo}</td>
+						<td>${e.ename}</td>
+						<td>${e.id}</td>
+						<td>${e.pwd}</td>
+						<td>${e.comm}</td>
+
+					</tr>
+				</c:forEach>
+			</table>
+			<button onclick="goEmpInsertPage()">사원등록</button>
+			
+		</c:if>
 	</div>
 </body>
 </html>
